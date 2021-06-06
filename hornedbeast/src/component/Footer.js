722ee699a43a52a394b9703bb93f23c20811deef
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-06-07 00:15:45 +0300

    some changes

---
+++ hornedbeast/src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component{   
+    render() {        
+        return(            
+        <footer>               
+             <span> Created By Ibrahim Alhamshari </span>           
+              </footer>       
+               )   
+             }}
+export default Footer;
